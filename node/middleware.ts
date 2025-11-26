f5d7024e487be5173943f0dcb59afef54fe603ee
Primefaya Computer Services <primefaya.computer.services@gmail.com>
2025-11-25 20:15:53 -0700

    dev_v1.0.0 ready

--- node/middleware.ts
+++ node/middleware.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import express from 'express';
 import {IN_DEV} from "./config";
 import apiRouter from "./api/v1";
-import websiteRouter from "./api/v1/website";
 
 const apiV1Router = express.Router();
 apiV1Router.use('/api/v1', apiRouter);
@@ -20,24 +19,11 @@ export function domainRouterWithLogger(req: Request, res: Response, next: NextFu
     if (IN_DEV && (host === 'localhost' || host === '192.168.1.70')) {
         if (path.startsWith('/api/v1')) {
             return apiV1Router(req, res, next);
-        } else {
-            req.app.locals.subdomain = 'localdev';
-            return websiteRouter(req, res, next);
         }
     }
 
-    if (host === 'api.dinersxpress.com') {
+    if (host === 'api.ceseelia.com') {
         return apiV1Router(req, res, next);
-    }
-
-    const domain = '.dinersxpress.com';
-    if (host.endsWith(domain)) {
-        const subdomain = host.slice(0, -domain.length);
-        const cleanSubdomain = subdomain.endsWith('.') ? subdomain.slice(0, -1) : subdomain;
-        if (cleanSubdomain && cleanSubdomain !== 'www') {
-            req.app.locals.subdomain = cleanSubdomain;
-            return websiteRouter(req, res, next);
-        }
     }
 
     res.status(404).send('Domain not recognized');
